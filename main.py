5885e2fe3cc457d7b8240d10ab81b29b7a60d9f8
Ci Leong Ong <ongcileong2001@gmail.com>
2023-02-08 03:30:27 +0800

    Add script with basic functionalities
    * Script behaviour is determined by `config.cfg`
    * Captured messages are dumped to `stdout`

---
+++ main.py
@@ -0,0 +1,37 @@
+#!usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" Telegram Bot: Selective Listener """
+
+from telethon import TelegramClient, events
+from pathlib import Path
+import yaml
+
+
+# Load the script configurations.
+
+PATH = Path("config.yaml")
+assert PATH.exists(), "Configuration file not found."
+
+with PATH.open(mode="r") as f:
+    cfg = yaml.safe_load(f)
+
+API_ID = cfg["api_id"]
+API_HASH = cfg["api_hash"]
+TARGETS = cfg["targets"]
+
+
+# Initialize client.
+client = TelegramClient("selective-listener", API_ID, API_HASH)
+
+
+@client.on(events.NewMessage(from_users=TARGETS))
+async def on_message_received(event):
+    print(event.message.message)
+
+
+# Initiate main loop.
+# Selectively listen for new messages until the client disconnect.
+
+client.start()
+client.run_until_disconnected()
